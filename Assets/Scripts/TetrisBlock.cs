89557ab064d636c7bba7023e819cd7ab8f753ef7
Alexey Kharchenko <doom9936.1999@gmail.com>
2019-11-11 03:35:44 +0200

    Add simple UI

---
+++ Assets/Scripts/TetrisBlock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+    public float fallTime = 0;
+    private float fallspeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckUserInput();
+    }
+
+    private void CheckUserInput()
+    {
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(0, 0, 90);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fallTime >= fallspeed)
+        {
+            transform.position += new Vector3(0, -1);
+
+            fallTime = Time.time;
+        }
+    }
+}
